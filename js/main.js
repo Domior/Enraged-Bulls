2a3236af35c839d385722f541ab41093eea94b57
Andrew Snisar <andrew.snisar@gmail.com>
2022-01-25 01:38:48 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const dropdown = document.querySelector('.dropdown');
+if ( dropdown != null) {
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+    dropdownMenu.style.display = 'none';
+
+    dropdownBtn.addEventListener('click', () => {
+        if (dropdownMenu.style.display === 'none') {
+            dropdownMenu.style.display = 'block';
+        } else {
+            dropdownMenu.style.display = 'none';
+        }
+    })
+
+    dropdownItems.forEach(el => {
+        el.addEventListener('click', () => {
+            dropdownBtn.textContent = el.textContent;
+            dropdownMenu.style.display = 'none';
+        })
+    })
+}
